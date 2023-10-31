5ad78a3cebd14197169ce7214b2f5740ddb50325
Poojitha SP <poojitha.sp@valtech.com>
2023-10-31 12:13:19 +0530

    commiting

--- first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
+++ first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
@@ -27,6 +27,10 @@ public class AppTest
     {
         return new TestSuite( AppTest.class );
     }
+    public void testSayHello() {
+    	App app= new App();
+    	assertEquals("Hello World",app.sayHello());
+    }
 
     /**
      * Rigourous Test :-)
